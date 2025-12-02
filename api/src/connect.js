568435e4ace789be2e6867fc67f3e9c9ab60cd08
Luiz Felipe Campos Margato <luizfelipe.camposmargato@gmail.com>
2025-12-01 23:27:13 -0300

    PAGINA ClicarAluno Finalizada -> ver padding-botom :)

--- api/src/connect.js
+++ api/src/connect.js
@@ -4,8 +4,8 @@ const pool = mysql.createPool({
   connectionLimit: 10, // Limite de conexões que suporta (10 conexões simultaneas)
   // 10.89.240.79 -> IP DO KAUANBGS :)
   host: "localhost", //Minha máquina (local) -> IP DE ONDE ESTÁ O BCD
-  user: 'root', //Usuário do MySQL
-  password: 'root', //Senha do MySQL
+  user: 'alunods', //Usuário do MySQL
+  password: 'senai@604', //Senha do MySQL
   database: 'carometro' //Mude para o seu database criado la no Shell/Workbench
 
 });
