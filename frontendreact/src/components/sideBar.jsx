84edd6ecceb4a28fae22d60a0684facb721a1b30
Kauan Plaza <kauanbgs13@gmail.com>
2026-03-05 13:17:57 -0300

    Sidebar finalizada

--- frontendreact/src/components/sideBar.jsx
+++ frontendreact/src/components/sideBar.jsx
@@ -1,11 +1,11 @@
-import { House } from 'lucide-react';
+import { House, UsersRound, GraduationCap, Shield } from 'lucide-react';
 
 
 export default function SideBar() {
     return (
-        <div className="w-52 bg-white border-r border-gray-200 flex flex-col h-screen fixed left-0 top-0 rounded-3xl">
+        <div className="w-[20%] bg-white border-r border-gray-200 flex flex-col h-screen fixed left-0 top-0 rounded-r-3xl">
             <header className="flex items-center justify-center p-4">
-                <img src="/SenaiLogo.png" alt="" className="w-48 " />
+                <img src="/SenaiLogo.png" alt="" className="w-42 mt-5 " />
             </header>
             <main className="flex flex-col gap-2 mt-12 p-4">
                 <div className="flex items-center gap-2">
@@ -15,14 +15,21 @@ export default function SideBar() {
                 <hr className="border-t-1 border-gray-800 mt-5 w-[85%] self-center" />
                 <h1 className="text-sm self-center text-blue-400 font-semibold mt-3">ALUNOS</h1>
                 <div className="flex items-center gap-2">
-                    <House />
+                    <UsersRound />
                     <h1 className="text-sm">Gerenciar Turmas</h1>
                 </div>
                 <hr className="border-t-1 border-gray-800 mt-5 w-[85%] self-center" />
                 <h1 className="text-sm self-center text-blue-400 font-semibold mt-3">DOCENTES</h1>
                 <div className="flex items-center gap-2">
-                    <House />
+                    <GraduationCap />
                     <h1 className="text-sm">Gerenciar Docentes</h1>
+                </div>
+                <hr className="border-t-1 border-gray-800 mt-5 w-[85%] self-center" />
+
+                <h1 className="text-sm self-center text-blue-400 font-semibold mt-3">DEV</h1>
+                <div className="flex items-center gap-2">
+                    <Shield />
+                    <h1 className="text-sm">Gerenciar Desenvolvedores</h1>
                 </div>
             </main>
         </div>
--- frontendreact/src/pages/Home/home.jsx
+++ frontendreact/src/pages/Home/home.jsx
@@ -2,8 +2,11 @@ import SideBar from "../../components/sideBar";
 
 export default function Home() {
     return (
-        <div>
+        <div className="bg-var(--background) h-screen w-screen bg-[var(--back)]">
             <SideBar />
+            <main className="flex-1 w-[80%] ml-[20%]">
+                <h1>Home</h1>
+            </main>
         </div>
     )
 }
