c8f0aad63676d3d9e4f4d664975f273674056319
Aluno <Luiz Felipe C Margato>
2026-03-12 12:49:03 -0300

    Adição do expo 54 e criação pagina login

--- mobileCarometro/App.js
+++ mobileCarometro/App.js
@@ -1,20 +1,20 @@
-import Login from "./src/screens/login";
-
-// npm install @react-navigation/native @react-navigation/stack
-// npm install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context react-native-vector-icons
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const Stack = createNativeStackNavigator();
-
   return (
-   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-    </Stack.Navigator>
-   </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
